ca8eab58a4c6c9b75ca47cb8cf83f866bf55a36e
league <league@adminisorsimac3.attlocal.net>
2025-01-02 16:58:32 -0800

    you will fail on the marvel quiz that I made for you! HAHAHAHAHAHA!!!!

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,15 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String weirdo = JOptionPane.showInputDialog("do you know how to write a code?");
+				
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(weirdo.equals("yes")) {
+	JOptionPane.showMessageDialog(null,"that means that you rock!");
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
-	}
+	}else{
+		  JOptionPane.showMessageDialog(null,"well that means you're coo-coo");
 }
-
+}
+}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,37 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
-		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+
+		// Create a variable to hold the user's score. Set it equal to zero.
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
-		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
-		// After all the questions have been asked, tell the user their final score 
-		
+
+		// 2. Ask the user a question
+		String answer1 = JOptionPane.showInputDialog("how do you unlock Ghost Rider in Lego Marvel Super Heroes?");
+		// 3. Use an if statement to check if their answer is correct
+		if (answer1.contains("circus")) {
+			JOptionPane.showMessageDialog(null, "correct!");
+			score  +=1;
+		} else {
+			JOptionPane.showMessageDialog(null, "wrong!");
+		}
+		String answer2 = JOptionPane.showInputDialog("who defeated Thanos in Avengers Endgame?");
+		if (answer2.contains("iron man")) {
+			JOptionPane.showMessageDialog(null, "correct!");
+			score  +=1;
+		} else {
+			JOptionPane.showMessageDialog(null, "wrong!");
+		}
+	String answer3 = JOptionPane.showInputDialog("in marvel, what is venom's weakness");
+		// 4. if the user's answer was correct, add one to their score
+
+		// MAKE MORE QUESTIONS. Ask more questions by repeating the above
+		// Option: Subtract a point from their score for a wrong answer
+
+		// After all the questions have been asked, tell the user their final score
+
 	}
 }
