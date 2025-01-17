193f57438a69747a99bd576a8f6b4cf38bc044e7
aloal <aloal@LAPTOP-GGF6A6OH>
2025-01-17 09:12:08 -0800

    scary stinky maze is done!

--- src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
+++ src/_04_amazing_games/_1_scary_maze/ScaryMaze.java
@@ -61,7 +61,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		// 5. Run your program and move your mouse over the START COLOR. A number will
 		// be printed to the console
 
-		int startColor = -7617718;
+		int startColor = -7617718; 
 		// 6. Change the value of this startColor variable to the number printed in the
 		// previous step.
 
